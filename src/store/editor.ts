512dc4b4a49629c8808cb8db10814d38157f7b23
frank <frank.wu@imile.me>
2021-07-07 14:00:53 +0800

    feature: 编写lText组件

--- src/store/editor.ts
+++ src/store/editor.ts
@@ -1,15 +1,80 @@
-import { createStore } from "vuex";
+import { Module } from "vuex";
 import { GlobalDataProps } from "./index";
+import { v4 as uuidv4 } from "uuid";
 
-interface EditorProps {
+export interface EditorProps {
   components: ComponentData[];
   currentElement: string;
 }
 
-interface ComponentData {
+export interface ComponentData {
   props: {
     [key: string]: any;
   };
   id: string;
   name: string;
 }
+
+export const testComponents: ComponentData[] = [
+  {
+    id: uuidv4(),
+    name: "l-text",
+    props: {
+      text: "hello",
+      fontSize: "30px",
+      color: "#000000",
+      lineHeight: "1",
+      textAlign: "left",
+      fontFamily: "",
+    },
+  },
+  {
+    id: uuidv4(),
+    name: "l-text",
+    props: {
+      text: "hello2",
+      fontSize: "10px",
+      fontWeight: "bold",
+      lineHeight: "2",
+      textAlign: "left",
+      fontFamily: "",
+    },
+  },
+  {
+    id: uuidv4(),
+    name: "l-text",
+    props: {
+      text: "hello3",
+      fontSize: "16px",
+      actionType: "url",
+      url: "https://www.baidu.com",
+      lineHeight: "3",
+      textAlign: "left",
+      fontFamily: "",
+    },
+  },
+];
+
+const editor: Module<EditorProps, GlobalDataProps> = {
+  state: {
+    components: testComponents,
+    currentElement: "",
+  },
+  mutations: {
+    addComponent(state, component: ComponentData) {
+      state.components.push(component);
+    },
+    setActive(state, currentId: string) {
+      state.currentElement = currentId;
+    },
+  },
+  getters: {
+    getCurrentElement: (state) => {
+      return state.components.find(
+        (component) => component.id === state.currentElement
+      );
+    },
+  },
+};
+
+export default editor;
--- src/store/index.ts
+++ src/store/index.ts
@@ -1,17 +1,20 @@
 import { createStore } from "vuex";
 import user, { UserProps } from "./user";
 import templates, { TemplatesProps } from "./templates";
+import editor, { EditorProps } from "./editor";
 
 // interface
 export interface GlobalDataProps {
   user: UserProps;
   templates: TemplatesProps;
+  editor: EditorProps;
 }
 
 const store = createStore({
   modules: {
     user,
     templates,
+    editor,
   },
 });
 
